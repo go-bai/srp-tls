b7acc4bbcfca789efe846aaf6c42055835daf221
go-bai <599500688@qq.com>
2022-09-07 19:40:19 +0800

    feat: 增加一键脚本

--- sh/install.sh
+++ sh/install.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
 
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
+
 if [ ! -d "/etc/srp-tls" ]; then
     mkdir /etc/srp-tls/ -p
 fi
 
-go mod tidy && go build -o srp-tls
-rm -f /etc/srp-tls/srp-tls
-mv srp-tls /etc/srp-tls/
+systemctl stop srp-tls
+
+wget -q -N -P /etc/srp-tls/ https://minio.gocn.top/public/app/srp-tls/srp-tls
+if [[ $? == 0 ]]; then
+    echo -e "${green}srp-tls 下载成功${plain}"
+else
+    echo -e "${red}srp-tls 下载失败${plain}"
+    exit 1
+fi
+
 
 cat > /etc/systemd/system/srp-tls.service << EOF
 [Unit]
